aa18efada570c620ac7df609ba2f2612bd7de90f
David Carvajal Garrido <davidcarvajalg@gmail.com>
2017-08-29 19:36:23 +0200

    Session component added to routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SessionComponent } from './session/session.component';
@@ -30,10 +32,17 @@ import { AlertModule } from 'ngx-bootstrap';
     SecondaryMenuComponent
   ],
   imports: [
-      AlertModule.forRoot(),
-    BrowserModule
+    AlertModule.forRoot(),
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'session',
+        component: SessionComponent
+      }
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
